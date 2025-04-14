0e9d0875feede39102b5b3c1571ba0568da378b4
Nguyen Dinh Bang <bangnd@abilive.vn>
2025-04-14 17:16:28 +0700

    layout skeleton (wip)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,54 +1,104 @@
-import { Layout } from 'antd';
+import React, { useState } from 'react';
+import { LaptopOutlined, NotificationOutlined, UserOutlined } from '@ant-design/icons';
+import type { MenuProps } from 'antd';
+import { Breadcrumb, Layout, Menu, theme } from 'antd';
 
-const { Header, Footer, Sider, Content } = Layout;
+const { Header, Content, Sider } = Layout;
 
-const headerStyle: React.CSSProperties = {
-  textAlign: 'center',
-  color: '#fff',
-  height: 64,
-  paddingInline: 48,
-  lineHeight: '64px',
-  backgroundColor: '#4096ff',
-};
+const items1: MenuProps['items'] = ['1', '2', '3'].map((key) => ({
+  key,
+  label: `nav ${key}`,
+}));
 
-const contentStyle: React.CSSProperties = {
-  textAlign: 'center',
-  minHeight: 120,
-  lineHeight: '120px',
-  color: '#fff',
-  backgroundColor: '#0958d9',
-};
+const items2: MenuProps['items'] = [UserOutlined, LaptopOutlined, NotificationOutlined].map(
+  (icon, index) => {
+    const key = String(index + 1);
 
-const siderStyle: React.CSSProperties = {
-  textAlign: 'center',
-  lineHeight: '120px',
-  color: '#fff',
-  backgroundColor: '#1677ff',
-};
+    return {
+      key: `sub${key}`,
+      icon: React.createElement(icon),
+      label: `subnav ${key}`,
+      children: Array.from({ length: 9 }).map((_, j) => {
+        const subKey = index * 9 + j + 1;
+        return {
+          key: subKey,
+          label: `option${subKey}`,
+        };
+      }),
+    };
+  },
+);
 
-const footerStyle: React.CSSProperties = {
-  textAlign: 'center',
-  color: '#fff',
-  backgroundColor: '#4096ff',
-};
+export function Home() {
+  const [collapsed, setCollapsed] = useState(false);
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken();
 
-const layoutStyle = {
-  overflow: 'hidden',
-};
-
-export function Home(){
   return (
-    <Layout style={layoutStyle}>
-      <Header style={headerStyle}>Header</Header>
+    <Layout style={{ minHeight: '100vh' }}>
+      <Header
+        style={{
+          position: 'sticky',
+          top: 0,
+          // zIndex: 1,
+          width: '100%',
+          display: 'flex',
+          alignItems: 'center',
+          background: 'transparent',
+        }}
+      >
+        <div className="demo-logo" />
+        <Menu
+          // theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={['2']}
+          items={items1}
+          style={{ flex: 1, minWidth: 0 }}
+        />
+      </Header>
       <Layout>
-        <Sider width="25%" style={siderStyle}>
-          Sider
+        <Sider width={200} collapsible collapsed={true} onCollapse={(value) => setCollapsed(value)}
+               trigger={null}
+          style={{
+            overflow: 'hidden',
+            height: '100vh',
+            position: 'sticky',
+            // insetInlineStart: 0,
+            top: 0,
+            bottom: 0,
+            scrollbarWidth: 'thin',
+            scrollbarGutter: 'stable',
+            scrollbarColor: 'transparent',
+          }}
+        >
+          <Menu
+            mode="vertical"
+            theme="dark"
+            defaultSelectedKeys={['1']}
+            defaultOpenKeys={['sub1']}
+            style={{ height: '100%', borderRight: 0 }}
+            items={items2}
+          />
         </Sider>
-        <Content style={contentStyle}>
-
-        </Content>
+        <Layout style={{ padding: '0 24px 24px' }}>
+          <Breadcrumb
+            items={[{ title: 'Home' }, { title: 'List' }, { title: 'App' }]}
+            style={{ margin: '16px 0' }}
+          />
+          <Content
+            style={{
+              padding: 24,
+              margin: 0,
+              minHeight: 1800,
+              background: colorBgContainer,
+              borderRadius: borderRadiusLG,
+            }}
+          >
+            Content
+          </Content>
+        </Layout>
       </Layout>
-      <Footer style={footerStyle}>Footer</Footer>
     </Layout>
   );
 }

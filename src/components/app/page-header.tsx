90156cee8b45084608286fd8f13e1267edf4a64c
Nguyen Dinh Bang <bangnd@abilive.vn>
2025-04-21 15:36:40 +0700

    feat: update font-family

--- src/components/app/page-header.tsx
+++ src/components/app/page-header.tsx
@@ -1,5 +1,8 @@
+
 export function PageHeader() {
   return (
-    <div>page header</div>
-  )
+      <div className="sticky top-12 flex shrink-0 items-center gap-2 border-b bg-background z-10 py-2 h-14">
+
+      </div>
+  );
 }

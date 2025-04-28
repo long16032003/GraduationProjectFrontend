2983cd95c4f73f5b46448831199bd3c831b84a8b
Dinh Bang <ndinhbang111@gmail.com>
2025-04-28 22:48:59 +0700

    feat: enhance HTTP request handling with CSRF token support and improved retry logic

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,5 +1,6 @@
 import { $fetch, FetchOptions, FetchRequest } from 'ofetch';
 import { defu } from 'defu';
+import { parse } from 'cookie-es';
 
 // https://velog.io/@hafnium1923/Access-Token-Refresh-Token
 // https://stackoverflow.com/questions/76798256/how-can-i-repeat-previous-request-in-ofetch
@@ -8,9 +9,9 @@ import { defu } from 'defu';
 // https://dev.to/neil585456525/enhance-user-experience-with-automatic-token-refresh-1013
 // https://github.com/SMAccess/ecommerce/blob/master/composables/useAuthRequest.ts
 
-
 export const defaultOptions = {
-  retry: 3,
+  baseURL: import.meta.env.VITE_API_URL,
+  retry: 1,
   // Default retry status codes:
   // 408 - Request Timeout
   // 409 - Conflict
@@ -20,20 +21,35 @@ export const defaultOptions = {
   // 502 - Bad Gateway
   // 503 - Service Unavailable
   // 504 - Gateway Timeout
-  retryStatusCodes: [408, 409, 425, 429, 500, 502, 503, 504],
-  retryDelay: 500, // delay between retries in ms
+  retryStatusCodes: [408, 409, 425, 429, 500, 502, 503, 504, 419],
+  // Delay between retries in milliseconds.
+  retryDelay: ({response}): number => {
+    if(response?.status === 419) {
+      // Retry immediately
+      return 0;
+    }
+    // Retry after 500ms
+    return 500;
+  },
   timeout: 10000, // 10s timeout
   credentials: 'same-origin',
   headers: {
     Accept: 'application/json',
   },
-  async onRequest() {
-
+  async onRequest({ options }) {
+    // automatically set x-xsrf-token header for request
+    // https://laravel.com/docs/12.x/sanctum#csrf-protection
+    const csrfToken = parse(document.cookie)['XSRF-TOKEN']
+    if (csrfToken) {
+      options.headers.set('x-xsrf-token', csrfToken)
+    }
   },
   async onResponseError({ response }) {
-    // Handle the response errors
+    // Handle "419 - CSRF token mismatch" error
+    // https://laravel.com/docs/12.x/sanctum#csrf-protection
     if (response.status === 419) {
-      //
+      // reinitialize XSRF-TOKEN cookie
+      await $http('/sanctum/csrf-cookie')
     }
   },
 } as FetchOptions;
